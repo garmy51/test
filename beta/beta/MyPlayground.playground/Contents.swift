0d5ae41ec1e428f14c96ddcad4acc638107d5eec
garmy51 <garmy517@gmail.com>
2022-04-11 21:36:37 +0900

    4/11 수업
    변수 선언, 사칙연산 , 데이터타입

---
+++ beta/beta/MyPlayground.playground/Contents.swift
@@ -0,0 +1,43 @@
+import UIKit
+
+//var greeting = "Hello, playground"
+////let a = ""
+////let a: String = "abc"
+//let abc: Int = 10
+//var a: String = "안녕하세요"
+//// 데이터 타입
+//// Int는 숫자
+//// String은 문자열
+//// Character는 문자
+//// Bool은 참과 거짓
+//var aplle: Bool = true
+//let 거짓: Bool = false
+//var banana: Int = 1000000
+// let array: Array<String> =
+
+// 더하기: +
+// 빼기: -
+// 나누기: /
+// 곱하기: *
+// 나머지: %
+
+var aaa: Int = 10
+//print(aaa + 5)
+aaa = aaa + 5
+
+var t: Int = 500
+t = t / 10
+print(t)
+
+var abc: Int = 100
+let hh: Int = 10
+abc = abc * hh
+print(abc)
+
+let c: Int = 10
+let q: Int = 10
+var y: Int = c + q
+print(y)
+
+
+
